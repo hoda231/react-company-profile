1500a5b26c6bbacf59b064c37dc024a4754666ee
hoda231 <hoda36388@gmail.com>
2026-01-05 09:21:28 +0200

    Add files via upload

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,39 +7,39 @@ import { FaInstagram } from "react-icons/fa";
 function Footer() {
   return (
     <footer class="footer">
-		<div class="container">
-			<div class="row">
-			<div class="footer-col">
-				<h4>get help</h4>
+    <div class="container">
+      <div class="row">
+      <div class="footer-col">
+        <h4>get help</h4>
 
              <ul>
 
              <li><a href="#">FAQ</a></li>
-			 <li><a href="#">shipping</a></li>
-			 <li><a href="#">returns</a></li>
-			 <li><a href="#">order status</a></li>
-			 <li><a href="#">payment options</a></li>
-			</ul>
-		</div>
-		<div class="footer-col">
-			<h4>online shop</h4>
-			<ul>
-				<li><a href="#">pizza</a></li>
-			
-			
-			</ul>
-		</div>
-		<div class="footer-col">
-			<h4>follow us</h4>
-			<div class="social-links">
-			 <a> <FaFacebookF  className="bag-icon" /></a>
-					<a ><FaYoutube /></a>
-					<a href="#"><FaInstagram /></a>
-			</div>
-			</div>
-			</div>
-		</div>
-	</footer>
+       <li><a href="#">our view</a></li>
+       <li><a href="#">contact</a></li>
+       <li><a href="#"> status</a></li>
+       <li><a href="#">payment options</a></li>
+      </ul>
+    </div>
+    <div class="footer-col">
+      <h4>company</h4>
+      <ul>
+        <li><a href="#">call us</a></li>
+      
+      
+      </ul>
+    </div>
+    <div class="footer-col">
+      <h4>follow us</h4>
+      <div class="social-links">
+       <a> <FaFacebookF  className="bag-icon" /></a>
+          <a ><FaYoutube /></a>
+          <a href="#"><FaInstagram /></a>
+      </div>
+      </div>
+      </div>
+    </div>
+  </footer>
   );
 }
 
